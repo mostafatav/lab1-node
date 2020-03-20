1441c49623947e90703d58e27ced1032e16f97c1
Mostafa Tavassoli <m.Tavassoli@asus.local>
2020-03-20 13:56:39 +0100

    the first EX is done

--- exercise0.js
+++ exercise0.js
@@ -1,3 +1,19 @@
 "use strict";
+function trimm (arrs){
+    for (var i=0; i<arrs.length;i++){
+        if (arrs[i].length <= 2){
+            totalArray[i] = '';
+            continue;
+        }else
+            totalArray[i] =  arrs[i].slice(0,2) + arrs[i].slice(arrs[i].length-2,arrs[i].length);  
+    }
+    return totalArray;
+}
 
-// here goes the code
+var st1 = 'MOSTAFA';
+var st2 = 'HOSEIN';
+var st3 = 'A'; 
+
+var totalArray = [st1,st2,st3];
+var an = trimm(totalArray);
+console.log(an);
