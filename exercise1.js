d571bc35be721124476c3592608d444d5b45841f
Mostafa Tavassoli <m.Tavassoli@asus.local>
2020-03-22 20:40:18 +0100

    Some Changes

--- exercise1.js
+++ exercise1.js
@@ -2,10 +2,12 @@
 var UIcontroller = (function(){
     return {
             showItems:function(){
-                console.log('Press 1 : insert a new task');
-                console.log('Press 2 : remove a task');
-                console.log('Press 3 : show all existing tasks, in alphabetic order');
-                console.log('Press 4 : close the program ');
+                console.log('----------------- TODO ITEMS --------------------');
+                console.log('Press 1 : Insert a new task');
+                console.log('Press 2 : Remove a task');
+                console.log('Press 3 : Show all existing tasks, in alphabetic order');
+                console.log('Press 4 : Close the program ');
+                console.log('-------------------------------------------------');
             },
             getInfo:function(txt){
                 var readlineSync = require('readline-sync');
@@ -13,7 +15,7 @@ var UIcontroller = (function(){
             },
     }
 })();
-var arrayTasks = new Array();
+var arrayTasks =[];
 var todoManager = function(){
     UIcontroller.showItems();
     function Tasks (desc, ur,pr,dead){
@@ -22,19 +24,19 @@ var todoManager = function(){
         this.pr=pr;
         this.dead = dead;
     }
-    var selectedItem = UIcontroller.getInfo('Please select the items : ');
+    var selectedItem = UIcontroller.getInfo('Select what do you want to DO : ');
     var intSelectedItem = parseInt(selectedItem);
         if ( intSelectedItem !== 4) {
             switch (intSelectedItem){
                 case 1:
                     var tsk = new Tasks();
-                    tsk.desc= UIcontroller.getInfo('Please Describe you Tasks : ');
+                    tsk.desc= UIcontroller.getInfo('Please Describe THE Task <Require> : ');
                     while(tsk.desc == ''){
-                        tsk.desc=UIcontroller.getInfo('Please Describe you Tasks : ');
+                        tsk.desc=UIcontroller.getInfo('Please Describe THE Task <Require> :');
                     }
-                    tsk.ur = UIcontroller.getInfo('Is that a Urgent Task : ? ') || 'no';
-                    tsk.pr =  UIcontroller.getInfo('Is that a Private Task : ? ') || 'private';
-                    tsk.dead = UIcontroller.getInfo('Enter the deadline for this task : ? ');
+                    tsk.ur = UIcontroller.getInfo('Is that an Urgent Task ? <Default : NO> ') || 'no';
+                    tsk.pr =  UIcontroller.getInfo('Is that a Private Task ? <Default : Private> ' ) || 'private';
+                    tsk.dead = UIcontroller.getInfo('Enter the deadline for this task <Optional> ');
                     arrayTasks.push(tsk);
                     break;
                 case 2:
